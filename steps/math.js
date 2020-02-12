ab847099f3185b216db9d64006e6f1df752071d6
sshaiakhmedov <e.shayakhmedov@gmail.com>
2020-02-11 22:38:47 -0700

    cucumber framework

--- steps/math.js
+++ steps/math.js
@@ -1,11 +1,11 @@
 const defineSupportCode = require('cucumber').defineSupportCode;
 
 defineSupportCode(function({ Given, Then, When }) {
-    Given('I start with {int}', function (int, callback) {
+    Given('I start with {0}', function (int, callback) {
         // Write code here that turns the phrase above into concrete actions
         callback(null, 'pending');
     });
-    When('I add {int}', function (int, callback) {
+    When('I add {1}', function (int, callback) {
         // Write code here that turns the phrase above into concrete actions
         callback(null, 'pending');
     });
